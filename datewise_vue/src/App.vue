7a5bd6311ad5d7c4c0423d127f127d67cce76bca
minji <alswl7754@gmail.com>
2024-06-11 16:05:18 +0900

    Feat : default setting / 김민지 / 20240611_1605

--- datewise_vue/src/App.vue
+++ datewise_vue/src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!--
+=========================================================
+* Vue Material Dashboard 2 PRO - v3.1.0
+=========================================================
 
+* Product Page: https://creative-tim.com/product/vue-material-dashboard-2-pro
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <!-- nav -->
+    <router-view />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+</script>
+
+<style>
+.dataTable-pagination-list .active a {
+  background-image: linear-gradient(
+    195deg,
+    #66bb6a 0%,
+    #43a047 100%
+  ) !important;
 }
 </style>
