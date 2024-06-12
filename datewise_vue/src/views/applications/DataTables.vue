8cde7a139597f83ed6f3676b046524443e065d15
YeSeul <ysbsjh2464@naver.com>
2024-06-12 13:52:42 +0900

    Feat: 검색 화면 / 이예슬 / 20240612_1352

---
+++ datewise_vue/src/views/applications/DataTables.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="table-responsive">
+    <table ref="dataTable" id="datatable-search" class="table table-flush">
+      <thead class="thead-light">
+        <tr>
+          <th>Category</th>
+          <th>Title</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <td>{{ item.category }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.amount }}원</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataTables",
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ datewise_vue/src/views/pages/Search.vue
@@ -0,0 +1,183 @@
+<template>
+    <div class="container">
+        <div class="py-4 container-fluid">
+            <div class="mt-4 row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="filter-container">
+                            <div class="filter-group period-group">
+                                <label class="label_txt">Set Period :</label>
+                                <div class="container text-left">
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-10">
+                                            <form action="#" class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <input type="date" id="startDate"
+                                                            class="datepicker form-control" placeholder="Start Date"
+                                                            v-model="filters.startDate">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <input type="date" id="endDate" class="datepicker form-control"
+                                                            placeholder="End Date" v-model="filters.endDate">
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="filter-group">
+                                <label class="label_txt">Type :</label>
+                                <div class="radio-group">
+                                    <input type="radio" id="spending" value="Spending" v-model="filters.type" checked>
+                                    <label for="spending">Spending</label>
+                                    <input type="radio" id="income" value="Income" v-model="filters.type">
+                                    <label for="income">Income</label>
+                                </div>
+                            </div>
+                            <hr v-if="filters.type">
+                            <div class="filter-group" v-if="filters.type === 'Spending'">
+                                <label class="label_txt">Payment method :</label>
+                                <div class="checkbox-group">
+                                    <input type="checkbox" id="debit" value="Debit card"
+                                        v-model="filters.paymentMethod">
+                                    <label for="debit">Debit card</label>
+                                    <input type="checkbox" id="credit" value="Credit card"
+                                        v-model="filters.paymentMethod">
+                                    <label for="credit">Credit card</label>
+                                    <input type="checkbox" id="cash" value="Cash" v-model="filters.paymentMethod">
+                                    <label for="cash">Cash</label>
+                                </div>
+                            </div>
+                            <hr v-if="filters.type === 'Spending' && filters.paymentMethod.length > 0">
+                            <div class="filter-group"
+                                v-if="(filters.type === 'Spending') && filters.paymentMethod.length > 0">
+                                <label class="label_txt">Category :</label>
+                                <div class="checkbox-group">
+                                    <input type="checkbox" id="food" value="food" v-model="filters.category">
+                                    <label for="food">Food</label>
+                                    <input type="checkbox" id="hospital" value="hospital" v-model="filters.category">
+                                    <label for="hospital">Hospital</label>
+                                    <input type="checkbox" id="subscribe" value="subscribe" v-model="filters.category">
+                                    <label for="subscribe">Subscribe</label>
+                                </div>
+                            </div>
+                            <div class="filter-group" v-if="filters.type === 'Income'">
+                                <label class="label_txt">Category :</label>
+                                <div class="checkbox-group">
+                                    <input type="checkbox" id="stock" value="stock" v-model="filters.category">
+                                    <label for="stock">Stock</label>
+                                    <input type="checkbox" id="bonus" value="bonus" v-model="filters.category">
+                                    <label for="bonus">Bonus</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="py-4 container-fluid">
+            <div class="mt-4 row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="mb-0">Result</h5>
+                        </div>
+                        <DataTables :data="filteredData" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import DataTables from '../applications//DataTables.vue'
+
+const today = new Date();
+const todayString = today.toISOString().split('T')[0];
+
+const filters = ref({
+    startDate: todayString,
+    endDate: todayString,
+    type: 'Spending',
+    paymentMethod: [],
+    category: []
+})
+
+const data = ref([
+    { type: 'Spending', paymentMethod: 'Debit card', category: 'food', title: 'restaurant', amount: '-20,000', date: '2024-06-01' },
+    { type: 'Spending', paymentMethod: 'Credit card', category: 'food', title: "Jack's yum", amount: '-45,000', date: '2024-06-02' },
+    { type: 'Spending', paymentMethod: 'Debit card', category: 'food', title: 'Kimbap Heaven', amount: '-37,000', date: '2024-06-03' },
+    { type: 'Income', category: 'stock', title: 'Stock Dividends', amount: '+5,000', date: '2024-06-04' },
+    { type: 'Income', category: 'bonus', title: 'Annual Bonus', amount: '+15,000', date: '2024-06-05' }
+])
+
+const filteredData = computed(() => {
+    return data.value.filter(item => {
+        return (!filters.value.startDate || new Date(item.date) >= new Date(filters.value.startDate)) &&
+            (!filters.value.endDate || new Date(item.date) <= new Date(filters.value.endDate)) &&
+            (filters.value.type === '' || item.type === filters.value.type) &&
+            (filters.value.paymentMethod.length === 0 || filters.value.paymentMethod.includes(item.paymentMethod)) &&
+            (filters.value.category.length === 0 || filters.value.category.includes(item.category))
+    })
+})
+</script>
+
+<style scoped>
+.container {
+    padding: 20px;
+    font-family: "DM Serif Display", serif;
+    font-weight: bold;
+    font-style: normal;
+}
+
+.filter-container {
+    padding: 30px;
+}
+
+.filter-group {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+.period-group {
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.radio-group,
+.checkbox-group {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+hr {
+    border: #380A15 solid 1px;
+}
+
+label {
+    color: #380A15;
+}
+
+.label_txt {
+    font-weight: bold;
+    color: #380A15;
+    font-size: 15pt;
+}
+
+h2 {
+    font-family: "DM Serif Display", serif;
+    font-weight: bold;
+    font-style: normal;
+    color: #380A15;
+    font-size: 20pt;
+}
+</style>
