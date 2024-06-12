26b5d1cb008c175f51eec6dadf3cca6270aca539
ingyeom <wjd1836@gmail.com>
2024-06-12 12:53:41 +0900

    Feat: 임시 사이드바 생성 및 Charts.vue 연동

--- datewise_vue/src/App.vue
+++ datewise_vue/src/App.vue
@@ -13,13 +13,64 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
 <template>
+  <sidenav
+    v-if="showSidenav"
+    :custom_class="color"
+    :class="[isRTL ? 'fixed-end' : 'fixed-start']"
+  />
+
+  <main
+    class="main-content position-relative max-height-vh-100 h-100 overflow-x-hidden"
+  >
     <!-- nav -->
+    <navbar
+      v-if="showNavbar"
+      :class="[isNavFixed ? navbarFixed : '', isAbsolute ? absolute : '']"
+      :color="isAbsolute ? 'text-white opacity-8' : ''"
+      :min-nav="navbarMinimize"
+    />
     <router-view />
+    
+  </main>
 </template>
-
 <script>
-</script>
+import Sidenav from "./examples/Sidenav";
+import Navbar from "@/examples/Navbars/Navbar.vue";
+import { mapMutations, mapState } from "vuex";
 
+export default {
+  name: "App",
+  components: {
+    Sidenav,
+    Navbar,
+  },
+  computed: {
+    ...mapState([
+      "isRTL",
+      "color",
+      "isAbsolute",
+      "isNavFixed",
+      "navbarFixed",
+      "absolute",
+      "showSidenav",
+      "showNavbar",
+      "showFooter",
+      "showConfig",
+      "hideConfigButton",
+    ]),
+  },
+  beforeMount() {
+    const sidenav = document.getElementsByClassName("g-sidenav-show")[0];
+
+    if (window.innerWidth > 1200) {
+      sidenav.classList.add("g-sidenav-pinned");
+    }
+  },
+  methods: {
+    ...mapMutations(["navbarMinimize"]),
+  },
+};
+</script>
 <style>
 .dataTable-pagination-list .active a {
   background-image: linear-gradient(
--- datewise_vue/src/router/index.js
+++ datewise_vue/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Charts from "../views/pages/Charts.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/pages/charts",
+    name: "Charts",
+    component: Charts,
   },
 ];
 
